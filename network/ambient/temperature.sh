e83c78fe5d77bbd869dd30a96b5711c9d862d18f
Wataru <xbee@dream.jp>
2016-06-12 11:14:24 +0900

    DATA生成部の簡略化（echoコピペ削除）

--- network/ambient/temperature.sh
+++ network/ambient/temperature.sh
@@ -3,21 +3,21 @@
 # [保存先=Ambient(https://ambidata.io/)]
 # Copyright (c) 2016 Wataru KUNINO
 
-AmbientChannelId=100                                            # チャネルID
-AmbientWriteKey="0123456789abcdef"                              # ライトキー(16桁)
-HOST="54.65.206.59"                                             # 送信先アドレス(変更不要)
-TEMP_OFFSET=30                                                  # CPUの温度上昇値(要調整)
-INTERVAL=10                                                     # 測定間隔
+AmbientChannelId=100                                        # チャネルID(Ambientで取得)
+AmbientWriteKey="0123456789abcdef"                          # ライトキー(16桁・同上)
+HOST="54.65.206.59"                                         # 送信先アドレス(変更不要)
+TEMP_OFFSET=30                                              # CPUの温度上昇値(要調整)
+INTERVAL=10                                                 # 測定間隔
 
-while true;do                                                   # 永久に繰り返し
-temp=`cat /sys/devices/virtual/thermal/thermal_zone0/temp`      # 温度を取得
-DEC=`expr ${temp} / 1000`                                       # 気温に換算(整数部)
-FRAC=`expr ${temp} / 100 - ${DEC} \* 10`                        # 気温に換算(小数部)
-DEC=`expr ${DEC} - ${TEMP_OFFSET}`                              # 温度補正
-echo "Temperature = ${DEC}.${FRAC}"                             # 温度測定結果の表示
-DATA=\"d1\"\:\"`echo ${DEC}.${FRAC}`\"                          # データ生成
-JSON="{\"writeKey\":\"${AmbientWriteKey}\",${DATA}}"            # JSON用のデータを生成
+while true;do                                               # 永久に繰り返し
+temp=`cat /sys/devices/virtual/thermal/thermal_zone0/temp`  # 温度を取得
+DEC=`expr ${temp} / 1000`                                   # 気温に換算(整数部)
+FRAC=`expr ${temp} / 100 - ${DEC} \* 10`                    # 気温に換算(小数部)
+DEC=`expr ${DEC} - ${TEMP_OFFSET}`                          # 温度補正
+echo "Temperature = ${DEC}.${FRAC}"                         # 温度測定結果の表示
+DATA=\"d1\"\:\"`echo ${DEC}.${FRAC}`\"                      # データ生成
+JSON="{\"writeKey\":\"${AmbientWriteKey}\",${DATA}}"        # JSON用のデータを生成
 curl -s ${HOST}/api/v2/channels/${AmbientChannelId}/data\
-     -X POST -H "Content-Type: application/json" -d ${JSON}     # データ送信
-sleep ${INTERVAL}                                               # 測定間隔の待ち時間
-done                                                            # 繰り返し
+     -X POST -H "Content-Type: application/json" -d ${JSON} # データ送信
+sleep ${INTERVAL}                                           # 測定間隔の待ち時間
+done                                                        # 繰り返し
