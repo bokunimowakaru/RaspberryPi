309579b6df3701cf969a41e1f37a6331320e2b64
Wataru <xbee@dream.jp>
2016-07-03 10:05:02 +0900

    バグ修正・未インストール起動時のエラー表示・10分ごとの送信など

--- network/i.myMimamoriPi/sub_i.MyMir.sh
+++ network/i.myMimamoriPi/sub_i.MyMir.sh
@@ -10,6 +10,20 @@ PID=`pidof raspi_ir_in`
 kill ${PID} >& /dev/null
 while true;do
 IR=`../../gpio/raspi_ir_in 4`
+if [ -z ${IR} ]; then
+	echo "GPIOエラー：インストールまたは接続、実行方法が不適切です"
+	echo
+	echo "(1) setup.shをしましたか？"
+	echo "　　cd ~/RaspberryPi/network/i.myMimamoriPi"
+	echo "　　./setup.sh"
+	echo "(2) GPIO 4 に赤外線受信モジュールを接続しましたか？"
+	echo "(3) 実行方法"
+	echo "　　cd ~/RaspberryPi/network/i.myMimamoriPi"
+	echo "　　./i.MyMimamoriPi.sh"
+	echo
+	echo "Ctrl-Cでプログラムを止めてください"
+	exit
+fi
 echo -n "[IR] "
 date +%d" "%H:%M |tr -d "\n" |tee -a ir.txt
 echo ", "${IR} |tee -a ir.txt
