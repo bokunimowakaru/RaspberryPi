5b1ce4395b0bb0586237a148ac965b5ef35dde91
Wataru KUNINO <xbee@dream.jp>
2016-11-11 19:08:12 +0900

    UDP送信機能を断念（Raspberry Piでのブロードキャスト送信の動作確認ができなかったため）

--- network/ambient/temperatureGet.sh
+++ network/ambient/temperatureGet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Ambientから温度データを受信し、UDPで送信する。ファイルへ保存する。
+# Ambientから温度データを受信し、ファイルへ保存する。
 # [データ取得元=Ambient(https://ambidata.io/)]
 # Copyright (c) 2016 Wataru KUNINO
 
@@ -9,9 +9,7 @@ HOST="54.65.206.59"                                     # 送信先アドレス(
 TEMP_OFFSET=25                                          # CPUの温度上昇値(要調整)
 INTERVAL=15                                             # 測定間隔
 DATE_="2000/01/01 00:00:00"                             # 前回の日付
-DEV="ambie_1"                                           # デバイス名を定義
-IP=`hostname -I|tr " " "\n"|grep -Eo '([0-9]*\.){3}[0-9]*'\
-|grep -v "127.0."|head -1|cut -d. -f1-3`                # IPアドレスを取得する
+DEV="ambie_1"                                           # デバイス名
 
 while true;do                                           # 永久に繰り返し
 JSON=`curl -s \
@@ -24,15 +22,16 @@ DATE=`echo $JSON\
     |tr "-" "/"\
     |tr "T" " "`                                        # 日時データを抽出
 TEMP=`echo $JSON|cut -d":" -f2|cut -d"," -f1`           # 温度データを抽出
-echo -n $DATE
-echo -n ", Temprature =" $TEMP                          # 取得結果を表示
-if [ "$DATE" != "$DATE_" ];then                         # 前回の日時と異なるとき
-    sudo echo -E ${DEV},$TEMP > /dev/udp/${IP}.255/1024 # UDPで送信する
-    echo -E ${DATE}, $TEMP >> log_${DEV}.csv            # ファイルへ保存する
-    DATE_="$DATE"                                       # 日時のバックアップ
-    echo " >>log_${DEV}.csv"                            # データ保存表示
-else
-    echo                                                # 改行
+if [ -n "$DATE" ];then                                  # データがあるとき
+    echo -n $DATE
+    echo -n ", Temprature =" $TEMP                      # 取得結果を表示
+    if [ "$DATE" != "$DATE_" ];then                     # 前回の日時と異なるとき
+        echo -E ${DATE}, $TEMP >> log_${DEV}.csv        # ファイルへ保存する
+        DATE_="$DATE"                                   # 日時のバックアップ
+        echo " >>log_${DEV}.csv"                        # データ保存表示
+    else
+        echo                                            # 改行
+    fi
 fi
 sleep ${INTERVAL}                                       # 測定間隔の待ち時間
 done                                                    # 繰り返し
