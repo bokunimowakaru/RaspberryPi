2d5e80ac19708ff4085350e8e4521d27521b5824
Wataru KUNINO <xbee@dream.jp>
2016-11-12 13:17:37 +0900

    UTC時刻の変換など

--- network/ambient/temperatureGet.sh
+++ network/ambient/temperatureGet.sh
@@ -3,13 +3,15 @@
 # [データ取得元=Ambient(https://ambidata.io/)]
 # Copyright (c) 2016 Wataru KUNINO
 
-AmbientChannelId=100                                    # チャネルID(Ambientで取得)
+AmbientChannelId=100                                    # チャネルID(Ambient用)
 AmbientReadKey="0123456789abcdef"                       # Readキー(16桁・同上)
-HOST="54.65.206.59"                                     # 送信先アドレス(変更不要)
-TEMP_OFFSET=25                                          # CPUの温度上昇値(要調整)
+HOST="ambidata.io"                                      # Ambientのアドレス
 INTERVAL=60                                             # 測定間隔
 DATE_="2000/01/01 00:00:00"                             # 前回の日付
 DEV="ambie_1"                                           # デバイス名
+if [ -f ~/.ambientkey ];then                            # 設定ファイル確認
+    source ~/.ambientkeys
+fi
 
 while true;do                                           # 永久に繰り返し
 JSON=`curl -s \
@@ -23,9 +25,10 @@ DATE=`echo $JSON\
     |tr "T" " "`                                        # 日時データを抽出
 TEMP=`echo $JSON|cut -d":" -f2|cut -d"," -f1`           # 温度データを抽出
 if [ -n "$DATE" ];then                                  # データがあるとき
-    echo -n $DATE
-    echo -n ", Temperature =" $TEMP                     # 取得結果を表示
+    DATE=`date --date "${DATE} UTC" "+%Y/%m/%d %R"`     # UTCをJST(等)へ変換
     if [ "$DATE" != "$DATE_" ];then                     # 前回の日時と異なるとき
+        echo -n $DATE                                   # 時刻を表示
+        echo -n ", Temperature =" $TEMP                 # 取得結果を表示
         echo -E ${DATE}, $TEMP >> log_${DEV}.csv        # ファイルへ保存する
         DATE_="$DATE"                                   # 日時のバックアップ
         echo " >>log_${DEV}.csv"                        # データ保存表示
