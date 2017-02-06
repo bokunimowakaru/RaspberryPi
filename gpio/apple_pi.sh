fc2afc9bfe954a2431d0dd38135bab566c1f1cec
Wataru KUNINO <xbee@dream.jp>
2017-02-06 22:18:25 +0900

    SW6をずっとおすとシャットダウン

--- gpio/apple_pi.sh
+++ gpio/apple_pi.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Apple Pi DEMO
+#
+# Copyright (c) 2017 Wataru KUNINO
+#
+# /etc/rc.local‚Ö‰º‹L‚ð’Ç‰Á‚·‚é‚ÆŽ©“®“I‚É‹N“®‚·‚é
+#       /home/pi/RaspberryPi/gpio/apple_pi.sh &
+
 while true; do
 /home/pi/RaspberryPi/gpio/raspi_lcd -i `hostname -I|cut -d" " -f1`
 /home/pi/RaspberryPi/gpio/raspi_gpo 5 1 >/dev/null
@@ -10,4 +18,16 @@ sleep 2
 /home/pi/RaspberryPi/gpio/raspi_lcd -i `date "+%y/%m/%d%R:%S"`
 /home/pi/RaspberryPi/gpio/raspi_gpo 6 0 >/dev/null
 sleep 2
+IN=`/home/pi/RaspberryPi/gpio/raspi_gpi 27 PUP`
+if [ $IN = "0" ]; then
+	/home/pi/RaspberryPi/gpio/raspi_lcd -i "shuting down..."
+	sleep 2
+	IN=`/home/pi/RaspberryPi/gpio/raspi_gpi 27 PUP`
+	if [ $IN = "0" ]; then
+		/home/pi/RaspberryPi/gpio/raspi_lcd -i "Bye."
+		sudo shutdown -h now
+		exit 0
+	fi
+	/home/pi/RaspberryPi/gpio/raspi_lcd -i "Canceled"
+fi
 done
