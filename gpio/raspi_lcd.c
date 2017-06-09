6b2a25eed42c835408071696fd780d065296dc85
Wataru KUNINO <xbee@dream.jp>
2017-06-09 20:17:45 +0900

    標準入力対応（暫定版）

--- gpio/raspi_lcd.c
+++ gpio/raspi_lcd.c
@@ -6,33 +6,31 @@ Raspberry Pi用 I2C液晶 表示プログラム  raspi_lcd
 
 I2C接続の小型液晶に文字を表示する
 
-                                        Copyright (c) 2014-2016 Wataru KUNINO
+                                        Copyright (c) 2014-2017 Wataru KUNINO
                                         http://www.geocities.jp/bokunimowakaru/
 *******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../libs/soft_i2c.h"
-
 typedef unsigned char byte;
 extern int ERROR_CHECK;
 
 int main(int argc,char **argv){
-    int i=1;
-    char s[49];
-    s[0]='\0';
+    int num=1; char s[49]; s[0]='\0';
     if(argc >=2 && argv[1][0]=='-'){
-        if(argc==2) strncat(s,"ｴﾗｰ ｦ ﾑｼ ｼﾃ ｿｳｼﾝ",47);
-        ERROR_CHECK=0;
-        i++;
+        ERROR_CHECK=0; num++;
     }
-    if(argc==1) strncat(s,"ﾎﾞｸﾆﾓﾜｶﾙ Rasp.Pi",47);
-    else while(i<argc && strlen(s)<16){
-        strncat(s,argv[i],47);
+    if(argc==num) fgets(s,sizeof(s),stdin);
+    else while(num<argc && strlen(s)<16){
+        strncat(s,argv[num],47);
         utf_del_uni(s);
         strncat(s," ",16);
-        i++;
+        num++;
+    }
+    if(strlen(s)==0){
+        if(ERROR_CHECK) strncat(s,"ｴﾗｰ ｦ ﾑｼ ｼﾃ ｿｳｼﾝ",47);
+        else strncat(s,"ﾎﾞｸﾆﾓﾜｶﾙ Rasp.Pi",47);
     }
     i2c_init();
     i2c_lcd_init();
