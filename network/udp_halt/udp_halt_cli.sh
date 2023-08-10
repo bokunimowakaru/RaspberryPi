2ff047b29cd3fa486fb5b4811ab05166fcfe504e
Wataru KUNINO <git@bokunimo.net>
2023-08-11 01:56:13 +0900

    Ping追加

--- network/udp_halt/udp_halt_cli.sh
+++ network/udp_halt/udp_halt_cli.sh
@@ -4,5 +4,22 @@
 
 cd `dirname $0`
 
-echo "UDP HALTER Client"         # タイトル表示
-echo "uhalt_0,1" | ../udp_sender.py                 # 送信実行
+echo "UDP HALTER Client"             # タイトル表示
+echo "HALTのブロードキャスト送信(255.255.255.255)"
+ping -c 1 -b 255.255.255.255 > /dev/null
+sleep 1
+echo "uhalt_0,1" | ../udp_sender.py
+sleep 1
+
+IP=`hostname -I|tr " " "\n"|grep -Eo '([0-9]*\.){3}[0-9]*'|grep -v "127.0."`
+for ip in $IP; do
+	broad_ip=`echo $ip|cut -d. -f1-3`".255"
+	echo "HALTのブロードキャスト送信("${broad_ip}")"
+	sleep 1
+	ping -c 1 -b ${broad_ip} > /dev/null
+	# echo "uhalt_0,1" | ../udp_sender_uni.py ${broad_ip} 1024 # 送信実行
+	echo "uhalt_0,1" | ../udp_sender.py
+	sleep 1
+	((i++))
+done
+
